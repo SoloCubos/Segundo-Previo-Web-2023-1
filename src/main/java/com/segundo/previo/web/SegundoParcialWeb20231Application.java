e035a8d6b161f46ecd06000467ec492f0e3911e5
Juan Solano <solanofernando35@gmail.com>
2023-05-29 10:19:37 -0500

    Commit Inicial

---
+++ src/main/java/com/segundo/previo/web/SegundoParcialWeb20231Application.java
@@ -0,0 +1,13 @@
+package com.segundo.previo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoParcialWeb20231Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoParcialWeb20231Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/segundo/previo/web/SegundoParcialWeb20231ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segundo.previo.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoParcialWeb20231ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
